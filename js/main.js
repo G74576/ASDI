ca6fe20f31510409b4789ff0a7d17d61104ea883
Kevin OToole <kmotoole78@fullsail.edu>
2013-06-11 22:07:50 -0400

    Added JSON.js file, Updated main.js file

--- js/main.js
+++ js/main.js
@@ -9,6 +9,38 @@ $('#homepage').on('pageinit', function(){
 
 $('#addpage').on('pageinit', function(){
 
+	// Save data to local storage function
+	
+	var saveData = function(key){
+		if(!key){
+			var uniqueId = Math.floor(Math.random()*100001);
+		}else{
+			uniqueId = key;
+		}
+		var item			={};
+			item.title		=['List Title:', $('#title').val()];
+			item.category	=['List Category:', $('#category').val()];
+			item.location	=['List Location:', $('#location').val()];
+			item.date		=['List Date:', $('#date').val()];
+			item.items		=['List Items:', $('items').val()];
+		localStorage.setItem(uniqueId, JSON.stringify(item));
+		alert('Your list has been saved!');
+	}
+	
+	function refreshWindo(){
+		window.location.reload():
+	}
+
+	// Get data from local storage function
+	
+	var getData = function(){
+		if(localStorage.length === 0){
+			alert('There are no lists in  local storage so default lists were added.');
+			autofillData();
+		}
+		var myUl = $('#displayContent');
+		$('#displayContent').style.display = 'block';
+	}
 
 });
 
